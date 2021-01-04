29ca49cfd043ab2d8ccc04a576455f872d9c2092
Magister <magisterbrownie@gmail.com>
2021-01-04 17:28:21 +0100

    Play button done

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,22 @@
 var c = document.getElementsByClassName("circuit")[0];
-let pointse = [new Point(0,0),new Point(20,36)];
-let combi = new Combiner(c,700,500);
+let combi = new Combiner(c,960,650);
+
+combi.addLine([new Point(0,120),new Point(240,120),new Point(280,190)]);
+combi.addLine([new Point(0,80),new Point(290,80),new Point(390,250)]);
+combi.addLine([new Point(0,30),new Point(360,30)]);
+combi.addLine([new Point(0,-15),new Point(280,-15)]);
+combi.addLine([new Point(0,-60),new Point(360,-60),new Point(400,-130)]);
+combi.addLine([new Point(0,-95),new Point(270,-95),new Point(325,-190),new Point(450,-190)]);
+combi.addLine([new Point(0,-130),new Point(210,-130),new Point(260,-220)]);
+combi.addLine([new Point(50,-100),new Point(100,-220),new Point(190,-220),new Point(230,-300)]);
+combi.addLine([new Point(-100,-100),new Point(-100,-230)]);
+combi.addLine([new Point(0,-110),new Point(-210,-110),new Point(-280,-280)]);
+combi.addLine([new Point(0,-70),new Point(-250,-70),new Point(-290,-160),new Point(-390,-160)]);
+combi.addLine([new Point(0,-35),new Point(-450,-35)]);
+combi.addLine([new Point(0,10),new Point(-370,10)]);
+combi.addLine([new Point(0,50),new Point(-230,50),new Point(-300,120)]);
+combi.addLine([new Point(0,95),new Point(-200,95)]);
+combi.addLine([new Point(0,130),new Point(-175,130),new Point(-220,200),new Point(-400,200)]);
 
 function Combiner(c,width,height){
     this.center = new Point(width/2,height/2);
@@ -29,7 +45,7 @@ function Line(points){
     this.draw = function(ctx){
         ctx.beginPath();
         ctx.strokeStyle = "#DC3545";
-        ctx.lineWidth = 10;
+        ctx.lineWidth = 13;
         ctx.moveTo(points[0].getX(),points[0].getY());
         for(let i=1;i<this.points.length;i++){
             ctx.lineTo(this.points[i].getX(),this.points[i].getY());
@@ -61,6 +77,6 @@ function Point(xpos,ypos){
     this.getY = function(){ return this.y};
     this.center = function(point){
         this.x+=point.getX();
-        this.y+=point.getY();
+        this.y=point.getY()-this.y;
     };
 }
