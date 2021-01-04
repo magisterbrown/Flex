3cb04018b8c18ebc512697f49e1ec89053da0042
Magister <magisterbrownie@gmail.com>
2021-01-04 02:37:37 +0100

    structure

--- public/js/script.js
+++ public/js/script.js
@@ -1,27 +1,35 @@
 var c = document.getElementsByClassName("circuit")[0];
-c.width=700;
-c.height=500;
-var ctx = c.getContext("2d");
-ctx.beginPath();
-ctx.moveTo(20, 20);
-ctx.lineTo(20, 100);
-ctx.lineTo(70, 100);
-ctx.stroke();
-let pointse = [new Point(0,0),new Point(200,360)];
-let poitne = new Point(10,10);
+let pointse = [new Point(0,0),new Point(20,36)];
+let combi = new Combiner(c,700,500);
+
+function Combiner(c,width,height){
+    this.center = new Point(width/2,height/2);
+    c.width = width;
+    c.height = height;
+    this.ctx = c.getContext("2d");
+    this.addLine = function(points){
+        for(let i=0;i<points.length;i++){
+           points[i].center(this.center); 
+        }
+        let ptx = new Path(points);
+        ptx.draw(this.ctx);
+    };
+
+}
 function Path(points){
    this.end = new Circle(points[points.length-1]);
    this.line = new Line(points); 
-   this.draw = function(center,ctx){
-       this.line.draw(center,ctx);
-       this.end.draw(center,ctx);
+   this.draw = function(ctx){
+       this.line.draw(ctx);
+       this.end.draw(ctx);
    };
 }
 function Line(points){
     this.points = points;
-    this.draw = function(center,ctx){
+    this.draw = function(ctx){
         ctx.beginPath();
         ctx.strokeStyle = "#DC3545";
+        ctx.lineWidth = 10;
         ctx.moveTo(points[0].getX(),points[0].getY());
         for(let i=1;i<this.points.length;i++){
             ctx.lineTo(this.points[i].getX(),this.points[i].getY());
@@ -34,17 +42,15 @@ function Line(points){
 function Circle(point){
     this.x = point.getX();
     this.y = point.getY();
-    this.draw = function(center,ctx){
+    this.draw = function(ctx){
+        this.drawOne(ctx,"#DC3545",22);
+        this.drawOne(ctx,"#FFF",12);
+    };
+    this.drawOne = function(ctx,color,size){
        ctx.beginPath(); 
-        ctx.arc(this.x, this.y, 22, 0, 2 * Math.PI);
+        ctx.arc(this.x, this.y, size, 0, 2 * Math.PI);
         ctx.lineWidth = 3;
-        ctx.fillStyle = '#DC3545';
-        ctx.fill();
-        
-        ctx.beginPath(); 
-        ctx.arc(this.x, this.y, 12, 0, 2 * Math.PI);
-        ctx.lineWidth = 3;
-        ctx.fillStyle = '#FFF';
+        ctx.fillStyle = color;
         ctx.fill();
     };
 }
@@ -53,4 +59,8 @@ function Point(xpos,ypos){
     this.y = ypos;
     this.getX = function(){ return this.x};
     this.getY = function(){ return this.y};
+    this.center = function(point){
+        this.x+=point.getX();
+        this.y+=point.getY();
+    };
 }
